2f3de7f636ea876227f6462a33ed9aeda698887d
DanFFFFF <daniellewfisher@gmail.com>
2024-11-14 18:56:30 +0100

    Created DF script for google cloud

---
+++ DF.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Download the PageRank data
+curl -o small_page_links.nt https://raw.githubusercontent.com/momo54/large_scale_data_management/main/small_page_links.nt
+
+# Variables to personalize
+PROJECT_ID="pagerank-441717"
+BUCKET_NAME="pagerank_bucket_100"
+CLUSTER_NAME="wordcount-cluster"
+REGION="us-central1"
+INPUT_FILE_PATH="small_page_links.nt"
+OUTPUT_PATH="output"
+ZONE="us-central1-a"
+PAGERANK_SCRIPT="DF.py"
+ITERATIONS=10
+
+# Enable necessary APIs
+gcloud services enable dataproc.googleapis.com
+gcloud services enable storage.googleapis.com
+
+# Configure the default project
+gcloud config set project $PROJECT_ID
+
+# Create a GCS bucket (if necessary)
+gsutil mb -l $REGION gs://$BUCKET_NAME/
+
+# Copy the input file to the bucket
+gsutil cp $INPUT_FILE_PATH gs://$BUCKET_NAME/
+
+
+# Download the PySpark PageRank script using DataFrames with a timer
+curl -o $PAGERANK_SCRIPT https://raw.githubusercontent.com/DanielFFFFF/PageRank2024/refs/heads/main/DF.py
+
+# Create a minimal single-node Dataproc cluster
+gcloud dataproc clusters create $CLUSTER_NAME \
+    --region=$REGION \
+    --zone=$ZONE \
+    --single-node \
+    --master-machine-type=n1-standard-2 \
+    --master-boot-disk-size=50GB \
+    --image-version=2.0-debian10
+
+# Submit the PySpark job to the cluster
+gcloud dataproc jobs submit pyspark $PAGERANK_SCRIPT \
+    --cluster=$CLUSTER_NAME \
+    --region=$REGION \
+    -- gs://$BUCKET_NAME/$INPUT_FILE_PATH $ITERATIONS
+
+# Wait for the job to finish
+sleep 10
+
+# List results in GCS
+gsutil ls gs://$BUCKET_NAME/$OUTPUT_PATH/
+
+# Delete the cluster after execution
+gcloud dataproc clusters delete $CLUSTER_NAME --region=$REGION --quiet
+
