7b98f97c43dd746f8bd2b971f1a0f219b2224f66
DanFFFFF <daniellewfisher@gmail.com>
2024-11-15 16:50:35 +0100

    DF

--- Automatic.sh
+++ Automatic.sh
@@ -6,6 +6,7 @@ curl -o small_page_links.nt https://raw.githubusercontent.com/momo54/large_scale
 # Variables to personalize
 PROJECT_ID="pagerank-441717"
 BUCKET_NAME="pagerank_bucket_100"
+CLUSTER_NAME="wordcount-cluster"
 REGION="us-central1"
 INPUT_FILE_PATH="small_page_links.nt"
 OUTPUT_PATH="output"
@@ -22,6 +23,10 @@ gcloud config set project $PROJECT_ID
 # Create a GCS bucket (if necessary)
 gsutil mb -l $REGION gs://$BUCKET_NAME/ || echo "Bucket already exists."
 
+# Download the PySpark PageRank script using DataFrames with a timer
+rm DF.py
+curl https://raw.githubusercontent.com/DanielFFFFF/PageRank2024/refs/heads/main/DF.py
+
 # Copy the input file to the bucket
 gsutil cp $INPUT_FILE_PATH gs://$BUCKET_NAME/
 
@@ -31,7 +36,6 @@ for PYSCRIPT in *.py; do
 
     # Loop through 1 to 4 nodes configuration
     for WORKERS in 1 2 3 4; do
-        CLUSTER_NAME="wordcount-cluster-${WORKERS}-workers-${PYSCRIPT%.py}"
         echo "Creating cluster with $WORKERS worker node(s) for script $PYSCRIPT..."
 
         if [ $WORKERS -eq 1 ]; then
